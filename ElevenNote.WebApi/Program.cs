56cf610460d2be389813931fb3c4a821ae965380
Austin Frick <austin.frick429@gmail.com>
2024-06-08 12:21:12 -0400

    created database

--- ElevenNote.WebApi/Program.cs
+++ ElevenNote.WebApi/Program.cs
@@ -1,11 +1,35 @@
+using ElevenNote.Data;
+using ElevenNote.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. 
+
+//Add connection string to DbContext
+builder.Services.AddControllers();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<UserEntity>(options =>
+{
+    //Password configuration, optional 
+    options.Password.RequiredLength = 4;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false; 
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+}); 
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(); 
+
+
 
 var app = builder.Build();
 
